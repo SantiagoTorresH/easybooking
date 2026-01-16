a3af3df6fe649e40fabba0fa777678a76c6ba299
SantiagoT <122948175+SantiagoTorresH@users.noreply.github.com>
2026-01-16 12:56:52 -0500

    Conexion a MongoDB Atlas

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors'); 
+const mongoose = require('mongoose')
+require('dotenv').config(); // lee el archivo .env 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Middleware 
 app.use(cors()); // permitir que cualquier aplicaion use este servidor, esta api 
+app.use(express.json());
+
+// conexion a la base de datos
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Conectado a MongoDB Atlas 🌐 exitosamente'))
+    .catch(err => console.error('Error al conectar:', err ));
 
 app.get('/', (req, res) => {
-  res.send('¡Hola! Mi servidor está vivo y funcionando 🚀');
+  res.send('¡Hola! Mi servidor está vivo y funcionando en la nube 🚀');
 });
 
 app.listen(PORT, () => {
