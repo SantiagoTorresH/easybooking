10736715088725a286c3d3c795a378d4da8f40ea
SantiagoT <122948175+SantiagoTorresH@users.noreply.github.com>
2026-01-16 11:28:06 -0500

    Habilitar CORS para conectar con React

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors()); // permitir que cualquier aplicaion use este servidor, esta api 
 
 app.get('/', (req, res) => {
   res.send('¡Hola! Mi servidor está vivo y funcionando 🚀');
